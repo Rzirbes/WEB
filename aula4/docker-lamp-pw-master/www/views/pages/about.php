22e5a70f79a8b474576d1516ddbf94565ae1e106
Rômulo Zirbes <62040520+Rzirbes@users.noreply.github.com>
2022-04-18 09:26:05 -0300

    Delete aula4/docker-lamp-pw-master directory

--- aula4/docker-lamp-pw-master/www/views/pages/about.php
+++
@@ -1,5 +0,0 @@
-<h1>Sobre PHP</h1>
-
-<p>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium inventore reiciendis optio esse numquam, nemo repellat cum reprehenderit vitae excepturi voluptate est, eos hic architecto expedita ducimus repellendus magni. Hic.
-</p>
--- aula4/docker-lamp-pw-master/www/views/pages/products.php
+++
@@ -1,5 +0,0 @@
-<h1>Minha products</h1>
-
-<p>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, doloremque? Ut dolor placeat minus asperiores aliquid explicabo facilis ullam dolorum laborum repudiandae aut fugiat, cum culpa, eveniet deserunt. Non, fuga.
-</p>
